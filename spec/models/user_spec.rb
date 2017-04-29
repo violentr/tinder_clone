16a3b091b20cb610129cc3c8ab2bf52e6ed01625
Deniss <den.b@itservicesgroup.co.uk>
2017-04-29 13:20:07 +0100

    Add Spec for User model
    User respond_to email, created_at,
    updated_at attributes.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe User, type: :model do
   it {is_expected.to respond_to(:email) }
   it {is_expected.to respond_to(:created_at) }
   it {is_expected.to respond_to(:updated_at) }
+  it {is_expected.to respond_to(:uploads) }
 end
