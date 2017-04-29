4ba63b8973425492b692e186fcc81a85d2c85604
Deniss <den.b@itservicesgroup.co.uk>
2017-04-29 20:53:29 +0100

    Add Handle file_upload and list imnages in index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,16 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!
+
   def index
+    @images = current_user.uploads
+  end
+
+  def upload
+    current_user.uploads.create(image_params)
+    redirect_to root_path
+  end
+
+  def image_params
+    params.require(:upload).permit(:image)
   end
 end
