1f75be0daa1864e30d17d546d3855e364ab2e171
Deniss <den.b@itservicesgroup.co.uk>
2017-04-29 22:46:46 +0100

    Add Paperclip attachment settings set to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :uploads
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
+    :url => "/avatar/:id/:style/:basename.:extension",
+                          :path => ":rails_root/public/avatar/:id/:style/:basename.:extension"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
