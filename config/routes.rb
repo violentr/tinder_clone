81a26d0f78cba69a6cb69d1b5237a90c487c9b70
Deniss <den.b@itservicesgroup.co.uk>
2017-04-29 20:53:29 +0100

    Add Handle file_upload and list imnages in index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  post '/welcome/upload', path: "/upload", to: "welcome#upload"
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
