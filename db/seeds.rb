ceb569690ac6ac6f05936f5a7d47786c95ee6e57
Deniss <den.b@itservicesgroup.co.uk>
2017-04-30 12:11:56 +0100

    Add Populate database with users
    Use https://randomuser.me API
    emails and person pics were added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+uri = URI('https://randomuser.me/api/?results=50')
+response = Net::HTTP.get(uri)
+output = JSON.parse(response)["results"]
+puts "Populating database with users .."
+output.each do |item|
+  user = User.create(email: item["email"], password: "password",
+                     password_confirmation: "password",
+                     confirmed_at: Date.today - 1.day)
+  user.uploads.create(image: item["picture"]["large"]) if user.persisted?
+end
