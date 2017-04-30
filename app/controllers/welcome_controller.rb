a7d4559e2e9497df469619d352228bb2337caa71
Deniss <den.b@itservicesgroup.co.uk>
2017-04-30 16:13:05 +0100

    Add Populate users with images from db
    Populate list of user images on
    welcome#index view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,15 +2,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @images = current_user.uploads
+    @users = User.includes(:uploads)
   end
 
-  def upload
-    current_user.uploads.create(image_params)
-    redirect_to root_path
-  end
-
-  def image_params
-    params.require(:upload).permit(:image)
-  end
 end
