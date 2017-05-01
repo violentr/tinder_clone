69a3ba30956ae07f9643e74d4fb7a31a734f5059
Deniss <den.b@itservicesgroup.co.uk>
2017-05-01 15:56:56 +0100

    Add Controller Users list all users except already liked
    Logic to list all users except already liked by current
    user, add #attitude filter users what was marked as liked.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def index
-    users = User.includes(:uploads)
+    likes = Like.pluck(:liked_user_id)
+    users = User.includes(:uploads) - User.find(likes)
     @user = users.first
   end
 
   def attitude
+    user = User.find(params[:attitude][:user_id])
+    Like.create(user_id: current_user.id, liked_user_id: user.id)
     redirect_to users_path
   end
 end
