380c7043b7e3b544265ee566db3471bb8d9c4e49
Deniss <den.b@itservicesgroup.co.uk>
2017-05-01 18:38:17 +0100

    Add Logic for creating likes and dislikes for user.
    List all users except already voted ones.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    users = User.includes(:uploads)
-    @user = users.first
+    @user = User.filter_voted_users.first
   end
 
   def attitude
+    liked = params[:like]
+    user = User.find(params[:attitude][:user_id])
+    if liked.present?
+      user.liked_by current_user, :vote_scope => 'like'
+    end
+    user.disliked_by current_user, :vote_scope => 'dislike'
     redirect_to users_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  acts_as_votable
+
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :uploads
@@ -9,4 +11,15 @@ class User < ActiveRecord::Base
     :url => "users/:id/avatar/:style/:basename.:extension",
                           :path => "users/:id/avatar/:style/:basename.:extension"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+
+  def self.filter_voted_users
+    includes(:uploads) - (filter_users_by("like") + filter_users_by("dislike"))
+  end
+
+
+  def self.filter_users_by(condition)
+    liked = ActsAsVotable::Vote.where(vote_scope: condition)
+    find(liked.pluck(:votable_id))
+  end
 end
