3210df933841fefaf1b144da9a087129f443d6bc
Deniss <den.b@itservicesgroup.co.uk>
2017-05-01 19:31:17 +0100

    Add Logic list users liked by current_user
    Welcome#index acting as user feed, show
    all user pictures already liked.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.includes(:uploads)
+    @users = User.filter_users_by("like")
   end
 
 end
